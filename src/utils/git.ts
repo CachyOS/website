8ee31d673489582e18d5e78aa773a105ec16b577
Vladislav Nepogodin <nepogodin.vlad@gmail.com>
2024-02-09 22:30:12 +0400

    👷 utils: use node:child_process instead of execa

--- src/utils/git.ts
+++ src/utils/git.ts
@@ -9,21 +9,16 @@
  */
 
 import { basename, dirname } from 'node:path';
-import { execaSync } from 'execa';
+import { spawnSync } from 'node:child_process';
 
 /** Custom error thrown when git is not found in `PATH`. */
 class GitNotFoundError extends Error {}
-
-/** Custom error thrown when the current file is not tracked by git. */
-class FileNotTrackedError extends Error {}
 
 /**
  * Fetches the git history of a file and returns a relevant commit date.
  * It gets the commit date instead of author date so that amended commits
  * can have their dates updated.
  *
- * @throws {GitNotFoundError} If git is not found in `PATH`.
- * @throws {FileNotTrackedError} If the current file is not tracked by git.
  * @throws Also throws when `git log` exited with non-zero, or when it outputs
  * unexpected text.
  */
@@ -36,52 +31,35 @@ export function getFileCommitDate(
 } {
     let git_path = '';
     {
-        const { stdout } = execaSync('which', ['git']);
+        const { stdout } = spawnSync('which', ['git'], {
+            encoding: 'utf-8',
+        });
         if (!stdout) {
             throw new GitNotFoundError(
                 `Failed to retrieve git history for "${file}" because git is not installed.`
             );
         }
-        git_path = stdout;
+        git_path = stdout.replace(/\n$/, '');
     }
-    const result = execaSync(
-        git_path,
-        [
-            'log',
-            `--format=%ct`,
-            '--max-count=1',
-            ...(age === 'oldest' ? ['--follow', '--diff-filter=A'] : []),
-            '--',
-            basename(file),
-        ],
-        {
-            cwd: dirname(file),
-        }
-    );
-    if (result.exitCode !== 0) {
-        throw new Error(
-            `Failed to retrieve the git history for file "${file}" with exit code ${result.exitCode}: ${result.stderr}`
-        );
+
+    const result = spawnSync(git_path, ['log', '--format=%ct', '--max-count=1', ...(age === 'oldest' ? ['--follow', '--diff-filter=A'] : []), '--', basename(file)], {
+        cwd: dirname(file),
+        encoding: 'utf-8',
+    });
+
+    if (result.error) {
+        throw new Error(`Failed to retrieve the git history for file "${file}": ${result.error}`);
     }
 
     const output = result.stdout.trim();
-
-    if (!output) {
-        throw new FileNotTrackedError(
-            `Failed to retrieve the git history for file "${file}" because the file is not tracked by git.`
-        );
-    }
-
     const regex = /^(?<timestamp>\d+)$/;
     const match = output.match(regex);
 
-    if (!match) {
-        throw new Error(
-            `Failed to retrieve the git history for file "${file}" with unexpected output: ${output}`
-        );
+    if (!match?.groups?.timestamp) {
+        throw new Error(`Failed to validate the timestamp for file "${file}"`);
     }
 
-    const timestamp = Number(match.groups!.timestamp);
+    const timestamp = Number(match.groups.timestamp);
     const date = new Date(timestamp * 1000);
 
     return { date, timestamp };
