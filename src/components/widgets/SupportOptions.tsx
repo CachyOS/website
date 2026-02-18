7b1fc190235a9aca59fbb9aa0b5b857c908fbc33
Anton Ždanov <anton@azdanov.dev>
2026-02-18 11:58:08 +0200

    feat: use btn style

--- src/components/widgets/SupportOptions.tsx
+++ src/components/widgets/SupportOptions.tsx
@@ -10,10 +10,7 @@ const SupportOptions = ({ children }: Readonly<Props>) => {
   return (
     <>
       <SupportModal isOpen={isOpen} onClose={() => setIsOpen(false)} />
-      <button
-        className="bg-slate-900 hover:bg-slate-700 focus:outline-hidden focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50 text-white font-semibold h-12 px-6 rounded-lg w-full flex items-center justify-center sm:w-auto dark:bg-sky-600 dark:highlight-white/20 dark:hover:bg-sky-700"
-        onClick={() => setIsOpen(true)}
-      >
+      <button className="btn btn-accent" onClick={() => setIsOpen(true)}>
         {children}
       </button>
     </>
