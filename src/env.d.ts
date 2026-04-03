1c2e018f377f72ffcb6d0d16589d910c6648f623
Anton Ždanov <anton@azdanov.dev>
2026-04-03 12:01:38 +0300

    chore: add fontsource import side-effect declaration

---
+++ src/env.d.ts
@@ -0,0 +1,4 @@
+// https://fontsource.org/docs/getting-started/faq#cannot-find-module-fontsourcefont-name-or-its-corresponding-type-declarations
+declare module '*.css';
+declare module '@fontsource/*' {}
+declare module '@fontsource-variable/*' {}
